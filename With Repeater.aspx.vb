54d55da54e34467b3994c7b6cb0c55ef37fecbe3
Rk <r.kprajapati215@gmail.com>
2017-06-15 13:43:39 +0530

    Code file for setting selected image main image
    this is an Code file for setting selected image main image using repeater control.

---
+++ With Repeater.aspx.vb
@@ -0,0 +1,34 @@
+﻿
+Partial Class Default2
+    Inherits System.Web.UI.Page
+
+    Protected Sub Unnamed1_ItemCommand(source As Object, e As RepeaterCommandEventArgs)
+        'FIND THE MAIN IMAGE CONTROL 
+
+        Dim mainimg As Image = DirectCast(e.Item.FindControl("Image1"), Image)
+
+        'FIND IMAGE CONTROL BASED ON COMMAND NAME OF IMAGEBUTTON CONTROL.
+        'FIRST WE FIND FOR "FIRST IMAGE BUTTON" AND COMMAND NAME IS 'F'.
+
+
+        If e.CommandName.Equals("F") Then
+            'DECLARE DATALISTITEM VARIABLE AND FIND ITEM INDEX OF SELECTED ITEM IN DATALIST.
+            Dim item As RepeaterItem = repeater.Items(e.Item.ItemIndex)
+            'FIND CONTROL OF IMAGEBUTTON IN ITEM VARIABLE..
+            Dim imgbtn As ImageButton = item.FindControl("ImageButton1")
+            'SIMPLY SET THE IMAGEURL TO MAIN IMAGE URL.
+            mainimg.ImageUrl = imgbtn.ImageUrl
+        End If
+        If e.CommandName.Equals("S") Then
+            'DECLARE DATALISTITEM VARIABLE AND FIND ITEM INDEX OF SELECTED ITEM IN DATALIST.
+            Dim item As RepeaterItem = repeater.Items(e.Item.ItemIndex)
+            'FIND CONTROL OF IMAGEBUTTON IN ITEM VARIABLE..
+            Dim imgbtn As ImageButton = item.FindControl("ImageButton2")
+            'SIMPLY SET THE IMAGEURL TO MAIN IMAGE URL.
+            mainimg.ImageUrl = imgbtn.ImageUrl
+        End If
+
+
+
+    End Sub
+End Class
